629283e42681e6973921b821d29b8574ee7e2569
Maks <maks@tarasutin.ru>
2024-03-18 16:34:12 +0300

    Заменил либу aiohttp на httpx

--- wbsellersnotifierbot/services/wb_keys.py
+++ wbsellersnotifierbot/services/wb_keys.py
@@ -1,5 +1,6 @@
-import aiohttp
+# import aiohttp
 from datetime import datetime
+import httpx
 from typing import List, Optional, Union
 from wbsellersnotifierbot.settings import settings
 
@@ -10,10 +11,9 @@ async def get_wb_keys(tg_user_id: int) -> dict:
     '''Функция возвращает список с API ключами пользователей
     '''
     try:
-        async with aiohttp.ClientSession() as client:
-            async with client.get(settings.url_api_service+f"api/wb_keys/get_wb_keys/{tg_user_id}/") as wb_keys:
-                data = await wb_keys.json()
-        return data
+        async with httpx.AsyncClient() as client:
+            wb_keys = await client.get(settings.url_api_service+f"api/wb_keys/get_wb_keys/{tg_user_id}/")
+        return wb_keys.json()
     except Exception as e:
         err_msg["text_error"] = e
         return err_msg
@@ -23,10 +23,9 @@ async def remove_wb_key(wb_key_id: int) -> dict:
     '''Функция удаляет API ключ пользователя
     '''
     try:
-        async with aiohttp.ClientSession() as client:
-            async with client.delete(settings.url_api_service+f"api/wb_keys/delete_wb_key/{wb_key_id}") as remove_wb_key:
-                data = await remove_wb_key.json()
-        return data
+        async with httpx.AsyncClient() as client:
+            remove_wb_key = await client.delete(settings.url_api_service+f"api/wb_keys/delete_wb_key/{wb_key_id}")
+        return remove_wb_key.json()
     except Exception as e:
         err_msg["text_error"] = e
         return err_msg
@@ -37,13 +36,12 @@ async def check_wb_key(api_key: str) -> dict:
     '''
     
     try:
-        async with aiohttp.ClientSession() as client: 
-            async with client.get(settings.url_wb_for_checking_key,
+        async with httpx.AsyncClient() as client: 
+            check_wb_key = await client.get(settings.url_wb_for_checking_key,
                                   headers={"Authorization": api_key},
                                   params={'dateFrom':datetime.now().strftime("%Y-%m-%d")}
-                                  ) as check_wb_key:
-                status_code = check_wb_key.status
-        return {"code": status_code}
+                                  )
+        return {"code": check_wb_key.status_code}
     except Exception as e:
         err_msg["text_error"] = e
         err_msg["code"] = 500
@@ -53,14 +51,14 @@ async def set_wb_key_for_user(tg_user_id: int, api_key: str, name_key: str|None)
     '''Функция возвращает данные после создания записи в БД
     '''
     try:
-        async with aiohttp.ClientSession() as client:     
-            async with client.post(settings.url_api_service+"api/wb_keys/set_wb_key",
+        async with httpx.AsyncClient() as client:     
+            set_wb_key_for_user = await client.post(settings.url_api_service+"api/wb_keys/set_wb_key",
                                    json={"user_telegram_id": tg_user_id,
                                            "api_key": api_key,
                                            "name_key": name_key
-                                           }) as set_wb_key_for_user:
-                data = await set_wb_key_for_user.json()
-                status_code = set_wb_key_for_user.status
+                                           })
+        data = await set_wb_key_for_user.json()
+        status_code = set_wb_key_for_user.status_code
         if status_code == 201 or status_code == 200:
             return data
         else:
